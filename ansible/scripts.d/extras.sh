5070a380e28d2b5349489d54f9b7a1a8e2e47d37
Daniel Enders <daniel@conception.bio>
2024-10-30 19:13:12 -0500

    Adding cuda venv and deps

--- ansible/scripts.d/extras.sh
+++ ansible/scripts.d/extras.sh
@@ -5,6 +5,9 @@ set -eo pipefail
 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+add-apt-repository -y ppa:deadsnakes/ppa
+chgrp -Rh 2345 /opt/venv
+chmod -R a+rwx /opt/venv/cuda-env
 apt-get update &&  apt-get install -y google-cloud-cli
 
 cd /opt/apps
